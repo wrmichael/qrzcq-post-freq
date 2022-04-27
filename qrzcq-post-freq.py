ba4c552ee2a4ad1cfa5c55d2bf21df2007b67ca9
Wayne Michael <wrmichael@hotmail.com>
2022-04-27 10:59:08 -0400

    Update qrzcq-post-freq.py

--- qrzcq-post-freq.py
+++ qrzcq-post-freq.py
@@ -12,7 +12,5 @@ if len(sys.argv)<=1:
 
 response = requests.post('https://ssl.qrzcq.com/api/rig/put', json={"auth":{"call":sys.argv[1],"key":sys.argv[2]},"request":{"mode":sys.argv[3],"freq":sys.argv[4],"smeter":sys.argv[5]}})
 
-if response.status == 0:
-	print(response.json())
-	quit()
-print("command line parameters: call key mode freq smeter")
+
+print(response.json())
